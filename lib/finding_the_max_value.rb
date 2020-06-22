c02c85762cac19f85868cfce5524b715bbbb14a4
Arnie Serrano <arniekidd@gmail.com>
2020-06-22 20:18:25 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,11 @@
 def find_max_value(array)
   # Add your solution here
-  
+  i=0
+  while i<array.length do
+    largest=0
+    if(array[i]>largest)
+      largest=array[i]
+    else
+      i+1
+  end
 end
